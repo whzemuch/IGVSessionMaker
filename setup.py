94ced4d8393ecf3fb50fc843195158e3b194202b
Hanzhou Wang <whzlp0430@yahoo.com>
2023-10-02 14:04:23 -0500

    update project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="make_xml_4_igv",
+    name="IGVSessionMaker",
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -14,5 +14,5 @@ setup(
     description="A package to generate IGV session files from CSV",
     license="MIT", # or other licenses you prefer
     keywords="igv xml bioinformatics",
-    url="https://github.com/yourusername/make_xml_4_igv",  # If you host your code on GitHub or another site
+    url="https://github.com/whzemuch/IGVSessionMaker/tree/main",  # If you host your code on GitHub or another site
 )
